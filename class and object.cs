1944c7e51bf76e853d613dc517788afcbc29efde
Manish Tomar <99877976+Pratapmanish25@users.noreply.github.com>
2022-04-10 23:19:30 +0530

    class and object

---
+++ class and object.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    //fields
+    class student
+    {
+        public int id { get; set; } 
+        public string name { get; set; }    
+        public string adress { get; set; }
+
+        public void showdetail()
+        {
+            Console.WriteLine("student information");
+            Console.WriteLine("id : {0}, name : {1}, adress : {2} ", id, name, adress);
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student st=new student();
+            st.id = 1;
+            st.name = "Manish Tomar";
+            st.adress = "Meerut";
+
+            st.showdetail();
+            Console.ReadLine();
+        }
+    }
+}
